863f607b8a4d2baaec8704876fb840ea6030ed8c
joannagyory <39591674+joannagyory@users.noreply.github.com>
2018-11-08 14:54:32 -0500

    Added more comments to the code

--- USR_US1_Weather.R
+++ USR_US1_Weather.R
@@ -2,18 +2,19 @@ library(readr)
 library(dplyr)
 library(lubridate)
 
+# Read in the temperature and precipitation data files
 temps <- read_csv("C:/Users/Joanna/Desktop/Analytics/Fall Semester/Watershed Analytics Project/Weather Data/Raw weather data/USR0000NWMT.csv")
 precips <- read_csv("C:/Users/Joanna/Desktop/Analytics/Fall Semester/Watershed Analytics Project/Weather Data/Raw weather data/US1NHCR0012.csv")
 
 
-
+# Pull the relevant columns from the precipitation and temperature data
 precips2 <- precips[c(3,4)]
 temps2 <- temps[c(3,4,6,8)]
 
 # Inner join on DATE
 df <- merge(precips2, temps2, by='DATE')
 
-
+# Re-order columns
 df3 <- df[c(1,3,5,4,2)]
 
 # Rename columns
@@ -23,4 +24,5 @@ colnames(df3)[3] <- 'MinTemp'
 colnames(df3)[4] <- 'MaxTemp'
 colnames(df3)[5] <- 'Precip'
 
+# Write to csv
 write.csv(df3, "C:/Users/Joanna/Desktop/Analytics/Fall Semester/Watershed Analytics Project/Weather Data/Raw weather data/USR_US1_Daily_Weather.csv", row.names = FALSE)
